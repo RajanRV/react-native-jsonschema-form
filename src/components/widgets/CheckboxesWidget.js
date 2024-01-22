afcd67b9d15d39c2e0648c4a41d0ef25c06d07c8
rajan <rajanvasani9@gmail.com>
2024-01-22 13:08:17 +0530

    checkbox widget updated to remove duplication and multi select

--- src/components/widgets/CheckboxesWidget.js
+++ src/components/widgets/CheckboxesWidget.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native'
 import CheckBox from 'react-native-check-box'
 import { getStyle } from '../../utils'
 import _ from 'lodash'
-function CheckboxsWidget(props){
+function CheckboxsWidget(props) {
   const {
     schema,
     id,
@@ -17,51 +17,60 @@ function CheckboxsWidget(props){
     styleSheet,
     enumOptions
   } = props;
-
-    let widgetStyle = (styleName) => getStyle(styleSheet, styleName, "CheckboxsWidget")
-    let checkboxs =_.map(props.options.enumOptions,(option) => {
-    let checked = value==option.value
-      return (
-        <View style={[styles.content, widgetStyle('content')]}
-              key={option.value}>
-            <CheckBox
-            id={id}
-            unCheckedImage={<View style={{ width: 22, height: 22, backgroundColor: 'white' }} />}
-            uncheckedCheckBoxColor={'#6DA1B7'}
-            checkedCheckBoxColor={'#6DA1B7'}
-            required={required}
-            disabled={disabled || readonly}
-            autoFocus={autofocus}
-            onClick={()=>{onChange(option.value)}}
-            isChecked={checked}
-            style={{ marginRight: 10, width: '10%' }}
-            />
-            <Text style={[styles.text, widgetStyle('text')]} >{option.label}</Text>
-            </View>
-      )
-      })
-          return (
-            <View style={[styles.container, widgetStyle('container')]}>
-            {checkboxs}
-          </View>
-          )
-       
-    
+  let uniqueItems = {}
+  for (let item of props.options.enumOptions) {
+    uniqueItems[item.value] = item
   }
+  props.options.enumOptions = Object.keys(uniqueItems).map(key => uniqueItems[key])
+  let widgetStyle = (styleName) => getStyle(styleSheet, styleName, "CheckboxsWidget")
+  let checkboxs = _.map(props.options.enumOptions, (option) => {
+    let checked = false
+    if (Array.isArray(value)) {
+      checked = value.includes(option.value)
+    } else {
+      checked = value == option.value
+    }
+    return (
+      <View style={[styles.content, widgetStyle('content')]}
+        key={option.value}>
+        <CheckBox
+          id={id}
+          unCheckedImage={<View style={{ width: 22, height: 22, backgroundColor: 'white' }} />}
+          uncheckedCheckBoxColor={'#6DA1B7'}
+          checkedCheckBoxColor={'#6DA1B7'}
+          required={required}
+          disabled={disabled || readonly}
+          autoFocus={autofocus}
+          onClick={() => { onChange(Array.isArray(value) ? (value.find(val => val === option.value) ? value.filter(val => val !== option.value) : [...value, option.value]) : option.value) }}
+          isChecked={checked}
+          style={{ marginRight: 10, width: '10%' }}
+        />
+        <Text style={[styles.text, widgetStyle('text')]} >{option.label}</Text>
+      </View>
+    )
+  })
+  return (
+    <View style={[styles.container, widgetStyle('container')]}>
+      {checkboxs}
+    </View>
+  )
+
+
+}
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     justifyContent: 'space-between',
-    marginTop:10
+    marginTop: 10
 
   },
-  content:{
-     flexDirection: "row",
-       minHeight:25,
-       width: '90%',
-       marginTop:10
-       },
+  content: {
+    flexDirection: "row",
+    minHeight: 25,
+    width: '90%',
+    marginTop: 10
+  },
   text: {
     width: '90%',
     textAlign: "left"
